804075a638ee373cb8f4222cf976561dcbe467c5
y-jin112 <robins2424@kakao.com>
2024-08-06 17:47:59 +0900

    skills, profile add

--- src/component/About.js
+++ src/component/About.js
@@ -1,85 +1,107 @@
 import "../css/about.css";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 import $ from "jquery";
 
-function About(){
+function About() {
+  AOS.init({
+    once: true,
+    duration: 1000,
+  });
 
-    
+  return (
+    <>
+      <main className="main_front_section" data-aos="fade-up">
+        <div className="about_title">
+          <h2>ABOUT ME</h2>
+        </div>
+        <div className="about_me">
+          <div className="profile_img">
+            <img src="img/profile.jpeg"></img>
+          </div>
+          <div className="profile_section">
+            <h3>
+              원하는 결과를 얻기 위해 도전을 마다하지 않는, <br />{" "}
+              신입프론트엔드 강예진입니다.
+            </h3>
+            <p>
+              {" "}
+              저는 활발하고 강한 사람입니다.
+              <br />
+              할일에 대해 끝까지 책임을 가지고, 주어진 상황을 인지하고 현실의
+              문제를 해결하는 것이 우선이라고 생각합니다.
+              <br />
+              항상 실수 하지 않기 위해 노력하고 자존심보다는 냉정한 평가를 받아
+              들이려고 합니다.
+            </p>
+            <p>
+              {" "}
+              평소 컴퓨터나 인터넷 기술에 대해 관심이 많았습니다.
+              <br />
+              진로에 대해 고민하다가 IT기술을 배우게 된다면 경쟁력이 있을거라
+              생각하고 프로그래밍 공부를 시작했습니다.
+              <br />
+              처음에는 책이나 유튜브를 통해 기본기를 공부하였고, 그 이후
+              국비지원 학원을 다니며 조금 더 깊이있는 공부를 배우게 되었습니다.
+            </p>
+          </div>
+        </div>
+        <div className="about_me_under">
+          <div className="archiving">
+            <div className="github">
+              <img src="img/GitHub-Mark-64px.png"></img>
+              <b>소스 코드 저장소</b>
+              <ul>
+                <li>과거 프로젝트, 프로그램, 앱의 소스코드</li>
+                <li>혼자서 코딩 연습을 위해 만든 토이프로젝트</li>
+                <li>그린 아카데미에서 수상한 과제소스 코드</li>
+              </ul>
+            </div>
+            <div className="notion">
+              <img src="img/notion2.png"></img>
+              <b>공부 지식 공유 목적의 기록소</b>
+              <ul>
+                <li>공부한 것을 진정한 나의 것으로 만들기 위한 기록</li>
+                <li>웹 개발자의 길을 걸으며 공부한 웹 관련 지식 정리</li>
+                <li>수강시 배운 수업들의 내용 복습겸 정리</li>
+              </ul>
+            </div>
+          </div>
 
-    AOS.init({
-        once: true,
-        duration: 1000
-    });
-
-
-  
-    
-    return(
-        <>
-         <main className='main_front_section'  data-aos="fade-up">
-                <div className='about_title'>
-                    <h2>ABOUT ME</h2>
-                </div>
-                <div className='about_me'>
-                    <div className='profile_img'>
-                        <img src='img/profile.jpeg'></img>
-                    </div>
-                    <div className='profile_section'>
-                        <h3>
-                        원하는 결과를 얻기 위해 도전을 마다하지 않는, <br/> 신입프론트엔드 강예진입니다.</h3>
-                        <p> 저는 활발하고 강한 사람입니다.<br/>
-                            할일에 대해 끝까지 책임을 가지고, 주어진 상황을 인지하고 
-                            현실의 문제를 해결하는 것이 우선이라고 생각합니다.<br/>
-                            항상 실수 하지 않기 위해 노력하고 자존심보다는 냉정한 평가를 받아 들이려고 합니다.
-                        </p>
-                        <p> 평소 컴퓨터나 인터넷 기술에 대해 관심이 많았습니다.<br/>
-                            진로에 대해 고민하다가 IT기술을 배우게 된다면 경쟁력이 있을거라 생각하고 
-                            프로그래밍 공부를 시작했습니다.<br/>
-                            처음에는 책이나 유튜브를 통해 기본기를 공부하였고, 그 이후 국비지원 학원을 다니며 
-                            조금 더 깊이있는 공부를 배우게 되었습니다.
-                        </p>
-                    </div>
-                </div>
-                <div className='about_me_under'>
-                    <div className='archiving'>
-                            <div className='github'>
-                                <img src='img/GitHub-Mark-64px.png'></img>
-                                <b>소스 코드 저장소</b>
-                                <ul>
-                                    <li>과거 프로젝트, 프로그램, 앱의 소스코드</li>
-                                    <li>혼자서 코딩 연습을 위해 만든 토이프로젝트</li>
-                                    <li>그린 아카데미에서 수상한 과제소스 코드</li>
-                                </ul>
-                            </div>
-                            <div className='notion'>
-                                <img src='img/notion2.png'></img>
-                                <b>공부 지식 공유 목적의 기록소</b>
-                                <ul>
-                                    <li>공부한 것을 진정한 나의 것으로 만들기 위한 기록</li>
-                                    <li>웹 개발자의 길을 걸으며 공부한 웹 관련 지식 정리</li>
-                                    <li>수강시 배운 수업들의 내용 복습겸 정리</li>
-                                </ul>
-                            </div>
-                    </div>
-                    <div className='profile_section'>
-                        <b>강예진 kang ye jin</b>
-                        <p>front-end developer</p>
-                        <b>AGE</b>
-                        <p>29세 (94.05.24)</p>
-                        <b>PHONE NUMBER</b>
-                        <p>010-9173-9021</p>
-                        <b>E-MAIL</b>
-                        <p>robins2424@kakao.com</p>
-                    </div>
-                    <div className='education'>
-                        <b>EDUCATION</b>
-                        <p>2021.11.08 ~ 2022.04.12 그린컴퓨터 아카데미</p>
-                        <p>[스마트웹&콘텐츠개발 플립드러닝] PWA제작 프론트엔드 개발</p>
-                    </div>
-                </div>
-            </main>
-        </>
-    );
+          <div className="profile_section">
+            <b>강예진 kang ye jin</b>
+            <p>front-end developer</p>
+            <b>AGE</b>
+            <p>94.05.24</p>
+            <b>PHONE NUMBER</b>
+            <p>010-9173-9021</p>
+            <b>E-MAIL</b>
+            <p>robins2424@kakao.com</p>
+          </div>
+          <div className="profile">
+            <div className="education">
+              <b>EDUCATION</b>
+              <p>2021.11.08 ~ 2022.04.12 그린컴퓨터 아카데미</p>
+              <p>[스마트웹&콘텐츠개발 플립드러닝] PWA제작 프론트엔드 개발</p>
+            </div>
+            <div className="education">
+              <b>PREVIOUS TEAM</b>
+              <p>
+                그린잇의 웹/앱 개발팀에서 사원으로 근무하며 웹/앱 B2B 사이트의
+                운영 및 관리,
+                <br /> 시스템 장애 처리, 기술 지원을 담당했습니다.
+                <br /> 1년 동안 C# ASP.NET MVC와 Webform 기반의 사이트에서
+                <br /> 탁월한 문제 해결 능력을 발휘하며 팀의 성과에 크게
+                기여했습니다.
+                <br />
+                웹앱 개발팀의 일원으로서, 안정적인 시스템 운영과
+                <br /> 향상된 사용자 경험 제공에 주력했습니다.
+              </p>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
 export default About;
--- src/component/Skill.js
+++ src/component/Skill.js
@@ -1,83 +1,107 @@
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "../css/skill.css";
 import $ from "jquery";
 
+function Skill() {
+  $(".hover").mouseleave(function () {
+    $(this).removeClass("hover");
+  });
 
+  return (
+    <>
+      <main className="skill_section" data-aos="fade-down">
+        <div className="skills">
+          <div className="skills_title">
+            <h2>SKILLS</h2>
+          </div>
+          <div className="skills_section">
+            <div className="front_end">
+              <div className="html">
+                <img src="img/html.png"></img>
+                <p>
+                  HTML, HTML5의 기본 구조를 이해하며 타이틀, 하이퍼링크,
+                  플러그인, 태그 등이 가능합니다. 또한, 웹표준 및 웹접근성을
+                  항상 고려하여 마크업을 작성합니다.
+                </p>
+              </div>
+              <div className="css">
+                <img src="img/css.png"></img>
+                <img src="img/sass.png"></img>
+                <p>
+                  CSS3의 기본 구조와 속성을 이해하며 외부 스타일 시트를 이용해
+                  다양한 구조와 스타일, 코드 구현이 가능, scss 문법에
+                  익숙합니다.
+                </p>
+              </div>
+              <div className="js_jquery">
+                <img src="img/javascript.png"></img>
+                <img src="img/jquery.png"></img>
+                <p>
+                  JavaScript와 jQuery의 기본 구조를 이해하며 활용할 수 있습니다.
+                  선택자, 오픈소스 활용이 가능합니다.
+                </p>
+              </div>
+              <div className="react">
+                <img src="img/react.png"></img>
+                <p>
+                  각 페이지를 분리하여 React Hooks, useEffect, 컴포넌트, state를
+                  사용할 수 있습니다.
+                </p>
+              </div>
+              <div className="react">
+                <img src="/img/csharp.png"></img>
+                <p>
+                  ASP.NET(MVC), ASP기반으로 만들어진 웹사이트를 유지보수,
+                  운영하였습니다.
+                </p>
+              </div>
+            </div>
 
-function Skill(){
+            <div className="git_zep">
+              <div className="version_control">
+                <img src="img/GitHub-Mark-64px.png"></img>
+                <p>
+                  작업내용과 작업시점을 확인할 수 있고,
+                  <br />전 버전으로 되돌려서 문제를 해결할 수 있습니다.
+                </p>
+              </div>
+              <div className="git">
+                <img src="img/git.png"></img>
+                <p>Git을 이용한 형상관리 및 flow에 대한 이해가 있습니다.</p>
+              </div>
+              <div className="communication">
+                <img src="img/zeplin2.png"></img>
+                <p>
+                  프로젝트를 생성 및 관련 디자인 <br /> 파일(화면)을 업로드 할
+                  수 있습니다.
+                </p>
+              </div>
+            </div>
 
-    $(".hover").mouseleave(
-        function () {
-          $(this).removeClass("hover");
-        }
-    );
-
-    return(
-        <>
-                 <main className='skill_section'  data-aos="fade-down">
-                <div className='skills'>
-                        <div className='skills_title'>
-                            <h2>SKILLS</h2>
-                        </div>
-                        <div className='skills_section'>
-                            <div className='front_end'>
-                                <div className='html'>
-                                    <img src='img/html.png'></img>
-                                    <p>HTML, HTML5의 기본 구조를 이해하며 타이틀,
-                                        하이퍼링크, 플러그인, 태그 등이 가능합니다.
-                                        또한, 웹표준 및 웹접근성을 항상 고려하여 마크업을 작성합니다.</p>
-                                </div>
-                                <div className='css'>
-                                    <img src='img/css.png'></img><img src='img/sass.png'></img>
-                                    <p>CSS3의 기본 구조와 속성을 이해하며 외부 스타일 시트를 이용해
-                                        다양한 구조와 스타일, 코드 구현이 가능, scss 문법에 익숙합니다.</p>
-                                </div>
-                                <div className='js_jquery'>
-                                    <img src='img/javascript.png'></img>
-                                    <img src='img/jquery.png'></img>
-                                    <p>JavaScript와 jQuery의 기본 구조를 이해하며 활용할 수 있습니다.
-                                        선택자, 오픈소스 활용이 가능합니다.</p>
-                                </div>
-                                <div className='react'>
-                                    <img src='img/react.png'></img>
-                                    <p>각 페이지를 분리하여 React Hooks, useEffect, 컴포넌트, state를 사용할 수  있습니다.</p>
-                                </div>
-                                </div>
-
-                                <div className='git_zep'>
-                                    <div className='version_control'>
-                                        <img src='img/GitHub-Mark-64px.png'></img>
-                                        <p>작업내용과 작업시점을 확인할 수 있고,<br />전 버전으로 되돌려서 문제를 해결할 수 있습니다.</p>
-                                    </div>
-                                    <div className='git'>
-                                        <img src='img/git.png'></img>
-                                        <p>Git을 이용한 형상관리 및 flow에 대한 이해가 있습니다.</p>
-                                    </div>
-                                    <div className='communication'>
-                                        <img src='img/zeplin2.png'></img>
-                                        <p>프로젝트를 생성 및 관련 디자인 <br /> 파일(화면)을 업로드 할 수 있습니다.</p>
-                                    </div>
-                                </div>
-
-                                <div className='deplayment'>
-                                    <div className='mysql'>
-                                        <img src='img/mysql.png'></img>
-                                        <p>SQL문을 이용해 데이터베이스를 만들고<br /> 
-                                        데이터베이스 내부에 테이블을 생성하고<br /> 
-                                        테이블에 데이터를 생성하고 읽기,변경,삭제 할 수 있습니다.</p>
-                                    </div>
-                                    <div className='firebase'>
-                                        <img src='img/firebase.png'></img>
-                                        <p>Storage를 통해 사진, 동영상등 여러컨텐츠들을 <br />
-                                        저장할 수 있는 작은 공간을 만든 경험이 있습니다.</p>
-                                    </div>
-                                </div>
-                        </div>
-                    </div>
-            </main>
-
-        </>
-    );
+            <div className="deplayment">
+              <div className="mysql">
+                <img src="img/mysql.png"></img>
+                <p>
+                  SQL문을 이용해 데이터베이스를 만들고
+                  <br />
+                  데이터베이스 내부에 테이블을 생성하고
+                  <br />
+                  테이블에 데이터를 생성하고 읽기,변경,삭제 할 수 있습니다.
+                </p>
+              </div>
+              <div className="firebase">
+                <img src="img/firebase.png"></img>
+                <p>
+                  Storage를 통해 사진, 동영상등 여러컨텐츠들을 <br />
+                  저장할 수 있는 작은 공간을 만든 경험이 있습니다.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
 export default Skill;
